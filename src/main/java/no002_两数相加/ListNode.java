ffaee211c1755258d6c58a0d8706f0181ad5df48
Mister24 <motivated24@hotmail.com>
2021-09-05 23:34:06 +0800

    feature(2 两数相加 easy): https://leetcode-cn.com/problems/add-two-numbers/

---
+++ src/main/java/no002_两数相加/ListNode.java
@@ -0,0 +1,28 @@
+/**
+ * @Company Mister24.com Inc.
+ * @Copyright Copyright (c) 2016-2021 All Rights Reserved.
+ */
+package no002_两数相加;
+
+/**
+ * @author mr.24
+ * @version Id: ListNode, v 1.0 2021/9/2 12:10 上午 Exp $$
+ */
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+}
