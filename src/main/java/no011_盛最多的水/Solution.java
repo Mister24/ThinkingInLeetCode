dfe81f15a1db577080534ae41bf6730605314f19
Mister24 <motivated24@hotmail.com>
2021-09-11 16:45:50 +0800

    feature(11 盛最多的水 medium): https://leetcode-cn.com/problems/container-with-most-water/

---
+++ src/main/java/no011_盛最多的水/Solution.java
@@ -0,0 +1,22 @@
+/**
+ * @Company Mister24.com Inc.
+ * @Copyright Copyright (c) 2016-2021 All Rights Reserved.
+ */
+package no011_盛最多的水;
+
+/**
+ * @author mr.24
+ * @version Id: Solution, v 1.0 2021/9/11 4:41 下午 Exp $$
+ */
+public class Solution {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1, max = 0;
+        while (left < right) {
+            max = height[left] < height[right] ?
+                    Math.max(max, (right - left) * height[left++]) :
+                    Math.max(max, (right - left) * height[right--]);
+        }
+
+        return max;
+    }
+}
