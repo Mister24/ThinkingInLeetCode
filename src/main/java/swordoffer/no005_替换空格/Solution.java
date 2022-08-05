46e2561416ad39acf940fef751fa0a83b9e34b0e
Mister24 <motivated24@hotmail.com>
2022-08-05 23:15:19 +0800

    feature(剑指 Offer 05. 替换空格 easy): https://leetcode.cn/problems/ti-huan-kong-ge-lcof/

---
+++ src/main/java/swordoffer/no005_替换空格/Solution.java
@@ -0,0 +1,46 @@
+package swordoffer.no005_替换空格;
+
+class Solution {
+    
+    public static void main(String[] args) {
+        System.out.println(replaceSpace("We are happy." * 100));
+    }
+    
+    public static String replaceSpace(String s) {
+        if (null == s) {
+            return s;
+        }
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                String prefix = s.substring(0, i);
+                String suffix = s.substring(i + 1);
+                s = prefix + "%20" + suffix;
+            }
+        }
+        
+        return s;
+    }
+    
+    /**
+     * 效率高
+     *
+     * @param s
+     * @return
+     */
+    public static String replaceSpace1(String s) {
+        if (null == s) {
+            return s;
+        }
+        
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (' ' == s.charAt(i)) {
+                stringBuilder.append("%20");
+            } else {
+                stringBuilder.append(s.charAt(i));
+            }
+        }
+        return stringBuilder.toString();
+    }
+}
