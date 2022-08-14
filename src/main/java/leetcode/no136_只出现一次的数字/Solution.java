447acc15098ae83e18ebabe5dcf56f0bd91ff227
Mister24 <motivated24@hotmail.com>
2022-08-14 23:10:25 +0800

    feature(leetcode 136. 只出现一次的数字 easy): https://leetcode.cn/problems/single-number/

---
+++ src/main/java/leetcode/no136_只出现一次的数字/Solution.java
@@ -0,0 +1,16 @@
+package leetcode.no136_只出现一次的数字;
+
+class Solution {
+    
+    public static void main(String[] args) {
+    }
+    
+    public static int singleNumber(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            res = res ^ nums[i];
+        }
+
+        return res;
+    }
+}
