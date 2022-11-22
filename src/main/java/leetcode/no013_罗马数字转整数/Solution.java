9c45a6241e173e3624f5a69ffc0c46a846653acb
Mister24 <motivated24@hotmail.com>
2022-11-23 00:25:19 +0800

    feature(leetcode 13. 罗马数字转整数 easy): https://leetcode.cn/problems/roman-to-integer/

---
+++ src/main/java/leetcode/no013_罗马数字转整数/Solution.java
@@ -0,0 +1,34 @@
+package leetcode.no013_罗马数字转整数;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    
+    public int romanToInt(String s) {
+        //        I             1
+        //        V             5
+        //        X             10
+        //        L             50
+        //        C             100
+        //        D             500
+        //        M             1000
+        Map<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() -1 && romanMap.get(s.charAt(i)) < romanMap.get(s.charAt(i + 1))) {
+                res -= romanMap.get(s.charAt(i));
+            } else {
+                res += romanMap.get(s.charAt(i));
+            }
+        }
+        return res;
+    }
+}
