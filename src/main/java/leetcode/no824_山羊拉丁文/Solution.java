2f7da79ae1383ebb2978d6a7ecfb8f044bdf54e7
Mister24 <motivated24@hotmail.com>
2022-11-23 23:48:59 +0800

    feature(leetcode 824. 山羊拉丁文 easy): https://leetcode.cn/problems/goat-latin/

---
+++ src/main/java/leetcode/no824_山羊拉丁文/Solution.java
@@ -0,0 +1,41 @@
+package leetcode.no824_山羊拉丁文;
+
+class Solution {
+    
+    public static void main(String[] args) {
+        System.out.println(toGoatLatin("The quick brown fox jumped over the lazy dog"));
+    }
+    
+    public static String toGoatLatin(String sentence) {
+        if (null == sentence) {
+            return null;
+        }
+        
+        String[] strings = sentence.split(" ");
+    
+        String res = "";
+        for (int i = 0; i < strings.length; i++) {
+            res += process(strings[i]);
+            for (int j = 0; j < i+1; j++) {
+                res += "a";
+            }
+            
+            res += " ";
+        }
+        
+        res.substring(0, res.length()-1);
+        
+        return res;
+    }
+    
+    static String process(String string) {
+        if ("aeiouAEIOU".indexOf(string.charAt(0))>=0) {
+            string = string + "ma";
+        } else {
+            char ch = string.charAt(0);
+            string = string.substring(1) + ch + "ma";
+        }
+        
+        return string;
+    }
+}
