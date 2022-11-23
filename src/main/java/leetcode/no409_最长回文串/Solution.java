2a79a1c667442176ebf5257ccbb9c5d76fe6549a
Mister24 <motivated24@hotmail.com>
2022-11-24 00:28:41 +0800

    feature(leetcode 409. 最长回文串 easy): https://leetcode.cn/problems/longest-palindrome/

---
+++ src/main/java/leetcode/no409_最长回文串/Solution.java
@@ -0,0 +1,44 @@
+package leetcode.no409_最长回文串;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+class Solution {
+    
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"));;
+    }
+    
+    public static int longestPalindrome(String s) {
+        if (s.length() == 0 || s.length() == 1) {
+            return s.length();
+        }
+        
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) != null) {
+                int num = map.get(s.charAt(i));
+                map.put(s.charAt(i), num + 1);
+            } else {
+                map.put(s.charAt(i), 1);
+            }
+        }
+    
+        AtomicInteger maxSingle=new AtomicInteger();
+        AtomicInteger sum = new AtomicInteger();
+        map.entrySet().stream().forEach(
+                item -> {
+                    if (item.getValue()%2==0) {
+                        sum.addAndGet(item.getValue());
+                    } else {
+                        if (maxSingle.get() < item.getValue()) {
+                            maxSingle.set(item.getValue());
+                        }
+                    }
+                }
+        );
+        
+        return sum.addAndGet(maxSingle.get());
+    }
+}
