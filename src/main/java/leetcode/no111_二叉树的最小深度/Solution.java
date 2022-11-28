5a672a9b12098e6c5c94349d477a88e4de2294be
Mister24 <motivated24@hotmail.com>
2022-11-29 00:06:50 +0800

    feature(leetcode 111. 二叉树的最小深度 easy): https://leetcode.cn/problems/minimum-depth-of-binary-tree/

---
+++ src/main/java/leetcode/no111_二叉树的最小深度/Solution.java
@@ -0,0 +1,23 @@
+package leetcode.no111_二叉树的最小深度;
+
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (null == root) {
+            return 0;
+        }
+    
+        if (null == root.left && null == root.right) {
+            return 1;
+        }
+        
+        int minLef = minDepth(root.left);
+        int minRig = minDepth(root.right);
+        
+        if (root.left == null || root.right == null) {
+            return minLef + minRig + 1;
+        }
+        
+        return Math.min(minLef, minRig) + 1;
+    }
+    
+}
