dd05a92e4307cf4d4921360f616270114f649333
Mister24 <motivated24@hotmail.com>
2022-12-02 00:13:16 +0800

    feature(leetcode 83. 删除排序链表中的重复元素 easy): https://leetcode.cn/problems/remove-duplicates-from-sorted-list/

---
+++ src/main/java/leetcode/no083_删除排序链表中的重复元素/Solution.java
@@ -0,0 +1,21 @@
+package leetcode.no083_删除排序链表中的重复元素;
+
+class Solution {
+    
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        
+        ListNode pos = head;
+        while (null != pos && null != pos.next) {
+            if (pos.val == pos.next.val) {
+                pos.next = pos.next.next;
+            } else {
+                pos = pos.next;
+            }
+        }
+        
+        return head;
+    }
+}
