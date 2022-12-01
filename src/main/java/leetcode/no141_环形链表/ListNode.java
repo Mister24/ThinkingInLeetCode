87fab96cb23e26f6008355f67912097897592a3e
Mister24 <motivated24@hotmail.com>
2022-12-02 00:25:48 +0800

    feature(leetcode 141. 环形链表 easy): https://leetcode.cn/problems/linked-list-cycle/

---
+++ src/main/java/leetcode/no141_环形链表/ListNode.java
@@ -0,0 +1,10 @@
+package leetcode.no141_环形链表;
+
+class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
