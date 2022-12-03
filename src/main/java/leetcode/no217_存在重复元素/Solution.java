569e3f45dcf33afc3f81b101cf9f765eb4c8b884
Mister24 <motivated24@hotmail.com>
2022-12-03 23:02:56 +0800

    feature(leetcode 217. 存在重复元素 easy): https://leetcode.cn/problems/contains-duplicate/

---
+++ src/main/java/leetcode/no217_存在重复元素/Solution.java
@@ -0,0 +1,24 @@
+package leetcode.no217_存在重复元素;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    
+    public boolean containsDuplicate(int[] nums) {
+        if (null == nums) {
+            return false;
+        }
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.get(nums[i]) != null) {
+                return true;
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+        
+        return false;
+    }
+}
