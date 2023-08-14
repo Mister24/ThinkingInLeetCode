76001868d9690ea63cbaf52a67d171b8286e29e1
Mister24 <motivated24@hotmail.com>
2023-08-14 13:44:33 +0800

    feature(leetcode 22. 括号生成 medium): https://leetcode.cn/problems/maximum-average-subarray-i/description/?envType=study-plan-v2&envId=leetcode-75

--- src/main/java/leetcode/no022_括号生成/Solution.java
+++ src/main/java/leetcode/no022_括号生成/Solution.java
@@ -2,8 +2,10 @@
  * @Company Mister24.com Inc.
  * @Copyright Copyright (c) 2016-2021 All Rights Reserved.
  */
+
 package leetcode.no022_括号生成;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,13 +13,35 @@ import java.util.List;
  * @version Id: Solution, v 1.0 2021/12/25 4:34 下午 Exp $$
  */
 public class Solution {
+    
     public static void main(String[] args) {
         System.out.println(generateParenthesis(1));
         System.out.println(generateParenthesis(2));
         System.out.println(generateParenthesis(3));
     }
-
+    
+    static List<String> DATA = new ArrayList<>();
     public static List<String> generateParenthesis(int n) {
-        return null;
+        DATA.clear();
+        dfs(n, 0, 0, "");
+        return DATA;
+        
     }
+    
+    public static void dfs(int n, int lc, int rc, String str) {
+        if (lc == n || rc == n) {
+            DATA.add(str);
+            return;
+        } else {
+            if (lc < n) {
+                dfs(n, lc + 1, rc, str + "(");
+            }
+            
+            if (rc < n && lc > rc) {
+                dfs(n, lc, rc + 1, str + ")");
+            }
+        }
+    }
+    
+    
 }
