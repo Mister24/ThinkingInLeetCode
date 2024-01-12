03c5d7f09831865a315d1293315866a290c2a8bb
Mister24 <motivated24@hotmail.com>
2024-01-12 22:16:33 +0800

    feature(leetcode 238. 除自身以外数组的乘积 medium): https://leetcode.cn/problems/product-of-array-except-self/description/?envType=study-plan-v2&envId=leetcode-75

---
+++ src/main/java/leetcode/no238_除自身以外数组的乘积/Solution.java
@@ -0,0 +1,24 @@
+package leetcode.no238_除自身以外数组的乘积;
+
+class Solution {
+    
+    public int[] productExceptSelf(int[] nums) {
+        int[] left = new int[nums.length];
+        left[0] = 1;
+        int[] right = new int[nums.length];
+        right[nums.length - 1] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            left[i] = nums[i-1] * left[i - 1];
+        }
+        for (int i = nums.length - 2; i >= 0; i--) {
+            right[i] = nums[i+1] * right[i + 1];
+        }
+        
+        int[] res = new int[nums.length];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = left[i] * right[i];
+        }
+        
+        return res;
+    }
+}
